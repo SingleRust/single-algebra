67d27f9df2c4b58577c9a7cbb35d43ea091545fa
Ian <ian.diks@uni-saarland.de>
2025-03-27 13:30:01 +0100

    Small clustering upgrade
    added variable similarity metric
    added working via ArrayD instead of Array2

--- src/clustering/similarity_network.rs
+++ src/clustering/similarity_network.rs
@@ -6,10 +6,11 @@ use crate::network::Network;
 use crate::similarity::SimilarityMeasure;
 use kiddo::{KdTree, SquaredEuclidean};
 use nalgebra_sparse::{CooMatrix, CsrMatrix};
-use ndarray::Array2;
+use ndarray::{Array2, ArrayD, ArrayViewD};
 use num_traits::{Float, FromPrimitive, ToPrimitive};
 use petgraph::graph::UnGraph;
 use std::collections::HashSet;
+use kiddo::traits::DistanceMetric;
 
 pub fn create_similarity_network<T, S>(
     data: &Array2<T>,
@@ -55,6 +56,70 @@ where
 
     Network::new_from_graph(graph)
 }
+
+pub fn build_knn_network_combined_matrix_arrayd<T, const K: usize, D>(
+    data: ArrayViewD<T>,
+    k: u64,
+) -> anyhow::Result<CsrMatrix<T>>
+where
+    T: Float + FromPrimitive + ToPrimitive + Send + Sync + Default + 'static,
+    T: num_traits::float::FloatCore,
+    T: std::fmt::Debug,
+    T: std::ops::AddAssign,
+    D: DistanceMetric<T, K>
+{
+    if data.ndim() != 2 {
+        return Err(anyhow::anyhow!("The input array has to be two dimensional in order to be used to build a knn network."));
+    }
+
+    let shape = data.shape();
+    let n_samples = shape[0] as u64;
+    let n_features = shape[1] as u64;
+
+    if (n_features as usize) < K {
+        return Err(anyhow::anyhow!("The data must have at least K features in order to be used for building a knn network."))
+    }
+
+    let mut kdtree: KdTree<T, K> = KdTree::new();
+
+    for i in 0..n_samples {
+        let mut point_array = [T::zero(); K];
+        for j in 0..K {
+            point_array[j] = *data.get([i as usize, j]).unwrap_or(&T::zero());
+        }
+        kdtree.add(&point_array, i);
+    }
+
+    let mut triplets = Vec::with_capacity((n_samples * k) as usize);
+
+    for i in 0..n_samples {
+        let mut query_array = [T::zero(); K];
+        for j in 0..K {
+            query_array[j] = *data.get([i as usize, j]).unwrap_or(&T::zero());
+        }
+
+        let neighbors = kdtree.nearest_n::<D>(&query_array, (k+1) as usize);
+
+        for neighbor in neighbors.iter().skip(1) {
+            if i <= neighbor.item {
+                let weight = (-neighbor.distance.sqrt()).exp();
+                triplets.push((i as usize, neighbor.item as usize, weight));
+            }
+        }
+    }
+
+    let coo = CooMatrix::try_from_triplets(
+        n_samples as usize,
+        n_samples as usize,
+        triplets.iter().map(|&(i, _, _)| i).collect(),
+        triplets.iter().map(|&(_, j, _)| j).collect(),
+        triplets.iter().map(|&(_, _, v)| v).collect(),
+    )
+        .map_err(|e| anyhow::anyhow!("Failed to create COO matrix: {}", e))?;
+
+    Ok(CsrMatrix::from(&coo))
+}
+
 
 pub fn build_knn_network_combined_matrix<T, const K: usize>(
     data: &Array2<T>,
