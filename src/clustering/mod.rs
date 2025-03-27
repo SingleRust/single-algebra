150214e0857ac6bc86938ec6e7ee49c88fdbf796
Ian <ian.diks@uni-saarland.de>
2025-03-27 13:45:34 +0100

    Version bump and fixed export issue

--- src/clustering/mod.rs
+++ src/clustering/mod.rs
@@ -4,4 +4,5 @@ pub use louvain::Louvain;
 pub(crate) mod similarity_network;
 pub use similarity_network::build_knn_network_combined_matrix;
 pub use similarity_network::build_knn_network_separate_matrix;
+pub use similarity_network::build_knn_network_combined_matrix_arrayd;
 pub use similarity_network::create_similarity_network;
