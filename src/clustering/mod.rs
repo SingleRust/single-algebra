828ed479e0fe5455142b31580b46ff5722d5342b
Ian <ian.diks@uni-saarland.de>
2025-04-29 13:26:52 +0000

    Moved clsutering to specialized libraries and to single-statistics

--- src/clustering/mod.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) mod leiden;
-pub(crate) mod louvain;
-pub use louvain::Louvain;
-pub use leiden::Leiden;
-pub(crate) mod similarity_network;
-pub use similarity_network::build_knn_network_combined_matrix;
-pub use similarity_network::build_knn_network_separate_matrix;
-pub use similarity_network::build_knn_network_combined_matrix_arrayd;
-pub use similarity_network::create_similarity_network;
--- src/local_moving/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod standard;
-pub(crate) mod parallel;
-pub(crate) mod fast; 
-pub(crate) mod merging;
